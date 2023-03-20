688a7abcb55434bedc74cfaee482a1d2f881ed10
ybandby <123816707+ybandby@users.noreply.github.com>
2023-03-20 19:03:42 +0800

    a

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("Hello hot-fix Test!");
         System.out.println("1111");
           System.out.println("pull Test");
+        System.out.println("pull Test2");
     }
 }
