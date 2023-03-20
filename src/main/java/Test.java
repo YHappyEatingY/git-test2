48be467766728dc1e7496b159cadb130d9ec3cd4
ybandby <2458438711@qq.com>
2023-03-20 18:34:29 +0800

    second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello git11!");
         System.out.println("Hello git11!");
-        System.out.println("Hello git11222!");
+        System.out.println("Hello guigu!");
     }
 }
